a143545d54cfbaebea50e68f5c79e4444a6559a6
zyczzh <651625937@qq.com>
2017-03-10 17:35:28 +0800

    111

--- week 2/03/随机测试.py
+++ week 2/03/随机测试.py
@@ -1,6 +1,6 @@
 import random
 setcolor=['A','B','C','D','E','F',1,2,3,4,5,6,7,8,9,0]
-color=random.sample(setcolor,6)#选者六个字符组合
+color=random.sample(setcolor,5)#选者六个字符组合
 a="#"
 for j in color:
     a=a+str(j)#拼接字符
