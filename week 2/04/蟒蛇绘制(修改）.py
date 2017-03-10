66d94f496e585b5396ac304aab0e186efffddbe0
zyczzh <651625937@qq.com>
2017-03-10 16:27:22 +0800

    彩色

--- week 2/04/蟒蛇绘制(修改）.py
+++ week 2/04/蟒蛇绘制(修改）.py
@@ -1,6 +1,13 @@
 import turtle
+import random
 def drawSnake(rad,angle,len,neckrad):
+    setcolor=['A','B','C','D','E','F',1,2,3,4,5,6,7,8,9,0]
     for i in range(len):
+        color=random.sample(setcolor,6)
+        a="#"
+        for j in color:
+            a=a+str(j)
+        turtle.pencolor(a)
         turtle.circle(rad,angle)
         turtle.circle(-rad,angle)
     turtle.circle(rad,angle/2)
@@ -11,7 +18,6 @@ def main():
     turtle.setup(1300,800,0,0)
     pythonsize=30
     turtle.pensize(pythonsize)
-    turtle.pencolor("blue")
-    turtle.seth(-10)#影响了蟒蛇的方向
+    turtle.seth(-40)#影响了蟒蛇的方向
     drawSnake(40,80,5,pythonsize/2)
 main()
